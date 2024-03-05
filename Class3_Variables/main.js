1c33a55e2a8843d7ef6beee45fe880890aa86e31
Sharmeen <162305655+SharmeenSamad@users.noreply.github.com>
2024-03-05 15:23:43 +0500

    Add files via upload

---
+++ Class3_Variables/main.js
@@ -0,0 +1,13 @@
+var course = "Governor Initiative GenAI-Engineer squad!";
+console.log(course, "\n");
+var mssg = "Very Informative and Latest Technology";
+console.log(mssg, "\n");
+var personName = " This is Sharmeen Zubair";
+var experience = 10;
+var newExperience = experience + 5;
+var profession = " Functional Tester";
+var x = null;
+console.log("Good Day, "
+    + personName + ", I have a "
+    + newExperience + " years of diversified experience"
+    + " and being a" + profession + ", I enjoy finding bugs.");
---
+++ Class3_Variables/main.ts
@@ -0,0 +1,13 @@
+var course: string = "Governor Initiative GenAI-Engineer squad!"
+console.log(course, "\n")
+var mssg: string = "Very Informative and Latest Technology"
+console.log(mssg, "\n")
+var personName: string = " This is Sharmeen Zubair";
+var experience: number = 10;
+var newExperience: number = experience + 5
+var profession: string = " Functional Tester"
+var x = null;
+console.log("Good Day, " 
++ personName + ", I have a " 
++ newExperience + " years of diversified experience" 
++ " and being a" + profession + ", I enjoy finding bugs.") 
